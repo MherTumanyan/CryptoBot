fa073b3ce03034e907abffe74fab3c05ff39b924
mher <mher.tumanyan@nrgaming.com>
2024-07-30 22:11:06 +0400

    Working code

--- dist/src/modules/core/providers/db.provider.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DBProviderToken = void 0;
-exports.DBProviderToken = Symbol('DBProviderToken');
-//# sourceMappingURL=db.provider.js.map
